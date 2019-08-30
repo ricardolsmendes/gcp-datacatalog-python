a6b79e825c86c5bedf098e0280e24949e20e5dd8
Ricardo Mendes <ricardolsmendes@gmail.com>
2019-08-30 00:12:59 -0300

    Use unittest.TestCase for strings formatting tests

--- tests/load_template_csv_test.py
+++ tests/load_template_csv_test.py
@@ -1,37 +1,34 @@
+from unittest import TestCase
+
 from load_template_csv import StringFormatter
 
 
-def test_format_string_to_snakecase_abbreviation():
-    assert StringFormatter.format_to_snakecase('AAA') == 'aaa'
-    assert StringFormatter.format_to_snakecase('AAA-AAA') == 'aaa_aaa'
+class StringFormatterTest(TestCase):
 
+    def test_format_string_to_snakecase_abbreviation(self):
+        self.assertEqual(StringFormatter.format_to_snakecase('AAA'), 'aaa')
+        self.assertEqual(StringFormatter.format_to_snakecase('AAA-AAA'), 'aaa_aaa')
 
-def test_format_string_to_snakecase_camelcase():
-    assert StringFormatter.format_to_snakecase('camelCase') == 'camel_case'
+    def test_format_string_to_snakecase_camelcase(self):
+        self.assertEqual(StringFormatter.format_to_snakecase('camelCase'), 'camel_case')
 
+    def test_format_string_to_snakecase_leading_number(self):
+        self.assertEqual(StringFormatter.format_to_snakecase('1 number'), '1_number')
 
-def test_format_string_to_snakecase_leading_number():
-    assert StringFormatter.format_to_snakecase('1 number') == '1_number'
+    def test_format_string_to_snakecase_repeated_special_chars(self):
+        self.assertEqual(StringFormatter.format_to_snakecase('repeated   special___chars'), 'repeated_special_chars')
 
+    def test_format_string_to_snakecase_whitespaces(self):
+        self.assertEqual(StringFormatter.format_to_snakecase(' no leading and trailing '), 'no_leading_and_trailing')
+        self.assertEqual(StringFormatter.format_to_snakecase('\nno leading and trailing\t'), 'no_leading_and_trailing')
 
-def test_format_string_to_snakecase_repeated_special_chars():
-    assert StringFormatter.format_to_snakecase('repeated   special___chars') == 'repeated_special_chars'
+    def test_format_string_to_snakecase_special_chars(self):
+        self.assertEqual(StringFormatter.format_to_snakecase('special!#@-_ chars'), 'special_chars')
+        self.assertEqual(StringFormatter.format_to_snakecase('! special chars ?'), 'special_chars')
 
+    def test_format_string_to_snakecase_unicode(self):
+        self.assertEqual(StringFormatter.format_to_snakecase(u'å ä ß é ö ü'), 'a_a_e_o_u')
 
-def test_format_string_to_snakecase_whitespaces():
-    assert StringFormatter.format_to_snakecase(' no leading and trailing ') == 'no_leading_and_trailing'
-    assert StringFormatter.format_to_snakecase('\nno leading and trailing\t') == 'no_leading_and_trailing'
-
-
-def test_format_string_to_snakecase_special_chars():
-    assert StringFormatter.format_to_snakecase('special!#@-_ chars') == 'special_chars'
-    assert StringFormatter.format_to_snakecase('! special chars ?') == 'special_chars'
-
-
-def test_format_string_to_snakecase_unicode():
-    assert StringFormatter.format_to_snakecase(u'å ä ß é ö ü') == 'a_a_e_o_u'
-
-
-def test_format_string_to_snakecase_uppercase():
-    assert StringFormatter.format_to_snakecase('UPPERCASE') == 'uppercase'
-    assert StringFormatter.format_to_snakecase('UPPER CASE') == 'upper_case'
+    def test_format_string_to_snakecase_uppercase(self):
+        self.assertEqual(StringFormatter.format_to_snakecase('UPPERCASE'), 'uppercase')
+        self.assertEqual(StringFormatter.format_to_snakecase('UPPER CASE'), 'upper_case')
