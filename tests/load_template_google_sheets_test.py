332d88fe70dac11a4dae2b7dd8a64521553801c5
Ricardo Mendes <ricardolsmendes@gmail.com>
2019-09-03 16:21:29 -0300

    Switch assetEqual params order in unit tests

--- tests/load_template_google_sheets_test.py
+++ tests/load_template_google_sheets_test.py
@@ -6,29 +6,29 @@ from load_template_google_sheets import StringFormatter
 class StringFormatterTest(TestCase):
 
     def test_format_string_to_snakecase_abbreviation(self):
-        self.assertEqual(StringFormatter.format_to_snakecase('AAA'), 'aaa')
-        self.assertEqual(StringFormatter.format_to_snakecase('AAA-AAA'), 'aaa_aaa')
+        self.assertEqual('aaa', StringFormatter.format_to_snakecase('AAA'))
+        self.assertEqual('aaa_aaa', StringFormatter.format_to_snakecase('AAA-AAA'))
 
     def test_format_string_to_snakecase_camelcase(self):
-        self.assertEqual(StringFormatter.format_to_snakecase('camelCase'), 'camel_case')
+        self.assertEqual('camel_case', StringFormatter.format_to_snakecase('camelCase'))
 
     def test_format_string_to_snakecase_leading_number(self):
-        self.assertEqual(StringFormatter.format_to_snakecase('1 number'), '1_number')
+        self.assertEqual('1_number', StringFormatter.format_to_snakecase('1 number'))
 
     def test_format_string_to_snakecase_repeated_special_chars(self):
-        self.assertEqual(StringFormatter.format_to_snakecase('repeated   special___chars'), 'repeated_special_chars')
+        self.assertEqual('repeated_special_chars', StringFormatter.format_to_snakecase('repeated   special___chars'))
 
     def test_format_string_to_snakecase_whitespaces(self):
-        self.assertEqual(StringFormatter.format_to_snakecase(' no leading and trailing '), 'no_leading_and_trailing')
-        self.assertEqual(StringFormatter.format_to_snakecase('\nno leading and trailing\t'), 'no_leading_and_trailing')
+        self.assertEqual('no_leading_and_trailing', StringFormatter.format_to_snakecase(' no leading and trailing '))
+        self.assertEqual('no_leading_and_trailing', StringFormatter.format_to_snakecase('\nno leading and trailing\t'))
 
     def test_format_string_to_snakecase_special_chars(self):
-        self.assertEqual(StringFormatter.format_to_snakecase('special!#@-_ chars'), 'special_chars')
-        self.assertEqual(StringFormatter.format_to_snakecase('! special chars ?'), 'special_chars')
+        self.assertEqual('special_chars', StringFormatter.format_to_snakecase('special!#@-_ chars'))
+        self.assertEqual('special_chars', StringFormatter.format_to_snakecase('! special chars ?'))
 
     def test_format_string_to_snakecase_unicode(self):
-        self.assertEqual(StringFormatter.format_to_snakecase(u'å ä ß é ö ü'), 'a_a_e_o_u')
+        self.assertEqual('a_a_e_o_u', StringFormatter.format_to_snakecase(u'å ä ß é ö ü'))
 
     def test_format_string_to_snakecase_uppercase(self):
-        self.assertEqual(StringFormatter.format_to_snakecase('UPPERCASE'), 'uppercase')
-        self.assertEqual(StringFormatter.format_to_snakecase('UPPER CASE'), 'upper_case')
+        self.assertEqual('uppercase', StringFormatter.format_to_snakecase('UPPERCASE'))
+        self.assertEqual('upper_case', StringFormatter.format_to_snakecase('UPPER CASE'))
