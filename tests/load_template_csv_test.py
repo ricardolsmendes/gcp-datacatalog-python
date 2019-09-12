67f25f657f15a5291059d0d8b83d14b337a40eab
Ricardo Mendes <30279287+ricardolsmendes@users.noreply.github.com>
2019-09-12 00:15:28 -0300

    Increase overall unit tests coverage (#3)

--- tests/load_template_csv_test.py
+++ tests/load_template_csv_test.py
@@ -1,34 +1,208 @@
+from io import StringIO
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import mock_open, patch
 
 from google.api_core.exceptions import PermissionDenied
 
-from load_template_csv import DataCatalogHelper, StringFormatter
+from load_template_csv import CSVFilesReader, DataCatalogHelper, StringFormatter, TemplateMaker
 
-_LOOKED_UP_DATACATALOG_CLIENT = 'load_template_csv.datacatalog_v1beta1.DataCatalogClient'
+_PATCHED_DATACATALOG_CLIENT = 'load_template_csv.datacatalog_v1beta1.DataCatalogClient'
+_PATCHED_DATACATALOG_HELPER = 'load_template_csv.DataCatalogHelper'
 
 
-@patch(f'{_LOOKED_UP_DATACATALOG_CLIENT}.__init__', lambda self, *args: None)
+@patch(f'{_PATCHED_DATACATALOG_HELPER}.__init__', lambda self, *args: None)
+class TemplateMakerTest(TestCase):
+
+    def test_constructor_should_set_instance_attributes(self):
+        self.assertIsNotNone(TemplateMaker().__dict__['_TemplateMaker__datacatalog_helper'])
+
+    @patch(f'{_PATCHED_DATACATALOG_HELPER}.create_tag_template')
+    @patch(f'{_PATCHED_DATACATALOG_HELPER}.tag_template_exists')
+    @patch('load_template_csv.CSVFilesReader.read_master')
+    def test_run_should_create_master_template_with_primitive_fields(
+            self, mock_read_master, mock_tag_template_exists, mock_create_tag_template):
+
+        mock_read_master.return_value = [['val1', 'val2', 'BOOL']]
+        mock_tag_template_exists.return_value = False
+
+        TemplateMaker().run(
+            files_folder=None,
+            project_id=None,
+            template_id='test-template-id',
+            display_name='Test Template')
+
+        mock_read_master.assert_called_once()
+        mock_tag_template_exists.assert_called_once()
+        mock_create_tag_template.assert_called_once()
+
+    @patch(f'{_PATCHED_DATACATALOG_HELPER}.create_tag_template', lambda self, *args: None)
+    @patch(f'{_PATCHED_DATACATALOG_HELPER}.tag_template_exists', lambda self, *args: False)
+    @patch('load_template_csv.CSVFilesReader.read_helper')
+    @patch('load_template_csv.CSVFilesReader.read_master', lambda self, *args: [['val1', 'val2', 'ENUM']])
+    def test_run_should_create_master_template_with_enum_fields(self, mock_read_helper):
+        mock_read_helper.return_value = [['helper_val1']]
+
+        TemplateMaker().run(
+            files_folder=None,
+            project_id=None,
+            template_id='test-template-id',
+            display_name='Test Template')
+
+        mock_read_helper.assert_called_once()
+
+    @patch(f'{_PATCHED_DATACATALOG_HELPER}.create_tag_template')
+    @patch(f'{_PATCHED_DATACATALOG_HELPER}.tag_template_exists', lambda self, *args: False)
+    @patch(f'{_PATCHED_DATACATALOG_HELPER}.delete_tag_template', lambda self, *args: None)
+    @patch('load_template_csv.CSVFilesReader.read_helper')
+    @patch('load_template_csv.CSVFilesReader.read_master', lambda self, *args: [['val1', 'val2', 'MULTI']])
+    def test_run_should_create_helper_template_for_multivalued_fields(
+            self, mock_read_helper, mock_create_tag_template):
+
+        mock_read_helper.return_value = [['helper_val1']]
+
+        TemplateMaker().run(
+            files_folder=None,
+            project_id=None,
+            template_id='test-template-id',
+            display_name='Test Template',
+            delete_existing=True)
+
+        mock_read_helper.assert_called_once()
+        self.assertEqual(2, mock_create_tag_template.call_count)  # Create the master and helper Templates.
+
+    @patch(f'{_PATCHED_DATACATALOG_HELPER}.create_tag_template')
+    @patch(f'{_PATCHED_DATACATALOG_HELPER}.tag_template_exists', lambda self, *args: False)
+    @patch('load_template_csv.CSVFilesReader.read_helper')
+    @patch('load_template_csv.CSVFilesReader.read_master', lambda self, *args: [['val1', 'val2', 'MULTI']])
+    def test_run_should_ignore_template_for_multivalued_fields_if_file_not_found(
+            self, mock_read_helper, mock_create_tag_template):
+
+        mock_read_helper.side_effect = FileNotFoundError()
+
+        TemplateMaker().run(
+            files_folder=None,
+            project_id=None,
+            template_id='test-template-id',
+            display_name='Test Template')
+
+        mock_read_helper.assert_called_once()
+        mock_create_tag_template.assert_called_once()  # Create only the master Template.
+
+    @patch(f'{_PATCHED_DATACATALOG_HELPER}.create_tag_template', lambda self, *args: None)
+    @patch(f'{_PATCHED_DATACATALOG_HELPER}.tag_template_exists', lambda self, *args: False)
+    @patch(f'{_PATCHED_DATACATALOG_HELPER}.delete_tag_template')
+    @patch('load_template_csv.CSVFilesReader.read_master', lambda self, *args: [['val1', 'val2', 'BOOL']])
+    def test_run_should_not_delete_existing_template_by_default(self, mock_delete_tag_template):
+        TemplateMaker().run(
+            files_folder=None,
+            project_id=None,
+            template_id='test-template-id',
+            display_name='Test Template')
+
+        mock_delete_tag_template.assert_not_called()
+
+    @patch(f'{_PATCHED_DATACATALOG_HELPER}.create_tag_template', lambda self, *args: None)
+    @patch(f'{_PATCHED_DATACATALOG_HELPER}.tag_template_exists', lambda self, *args: False)
+    @patch(f'{_PATCHED_DATACATALOG_HELPER}.delete_tag_template')
+    @patch('load_template_csv.CSVFilesReader.read_master', lambda self, *args: [['val1', 'val2', 'BOOL']])
+    def test_run_should_delete_existing_template_if_flag_set(self, mock_delete_tag_template):
+        TemplateMaker().run(
+            files_folder=None,
+            project_id=None,
+            template_id='test-template-id',
+            display_name='Test Template',
+            delete_existing=True)
+
+        mock_delete_tag_template.assert_called_once()
+
+
+class CSVFilesReaderTest(TestCase):
+
+    @patch('builtins.open', new_callable=mock_open())
+    def test_read_master_should_return_content_as_list(self, mock_builtins_open):
+        mock_builtins_open.return_value = StringIO(
+            'col1,col2,col3\n'
+            'val1,val2,val3\n'
+        )
+
+        content = CSVFilesReader.read_master('test-folder', 'test-file-id')
+
+        mock_builtins_open.assert_called_with('test-folder/test-file-id.csv', mode='r')
+        self.assertEqual(1, len(content))  # The first line (header) is ignored.
+        self.assertEqual(3, len(content[0]))
+        self.assertEqual('val2', content[0][1])
+
+    @patch('builtins.open', new_callable=mock_open())
+    def test_read_helper_should_return_content_as_list(self, mock_builtins_open):
+        mock_builtins_open.return_value = StringIO(
+            'col1\n'
+            'val1\n'
+        )
+
+        content = CSVFilesReader.read_helper('test-folder', 'test-file-id')
+
+        mock_builtins_open.assert_called_with('test-folder/test-file-id.csv', mode='r')
+        self.assertEqual(1, len(content))  # The first line (header) is ignored.
+        self.assertEqual(1, len(content[0]))
+        self.assertEqual('val1', content[0][0])
+
+    @patch('builtins.open', new_callable=mock_open())
+    def test_read_should_return_exact_number_values_per_line(self, mock_builtins_open):
+        mock_builtins_open.return_value = StringIO(
+            'col1,col2,col3\n'
+            'val1,val2,val3\n'
+        )
+
+        content = CSVFilesReader.read_master(None, None, values_per_line=2)
+
+        self.assertEqual(2, len(content[0]))
+
+    @patch('builtins.open', new_callable=mock_open())
+    def test_read_should_return_stripped_content(self, mock_builtins_open):
+        mock_builtins_open.return_value = StringIO(
+            'col1,col2,col3\n'
+            'val1, val2  ,val3\n'
+        )
+
+        self.assertEqual('val2', CSVFilesReader.read_master(None, None)[0][1])
+
+
+@patch(f'{_PATCHED_DATACATALOG_CLIENT}.__init__', lambda self, *args: None)
 class DataCatalogHelperTest(TestCase):
 
-    @patch(f'{_LOOKED_UP_DATACATALOG_CLIENT}.delete_tag_template')
+    @patch(f'{_PATCHED_DATACATALOG_CLIENT}.create_tag_template')
+    def test_create_tag_template_should_handle_described_fields(self, mock_create_tag_template):
+        DataCatalogHelper().create_tag_template(
+            project_id=None,
+            template_id=None,
+            display_name='Test Display Name',
+            fields_descriptors=[
+                ['test-string-field-id', 'Test String Field Display Name', 'STRING'],
+                ['test-enum-field-id', 'Test ENUM Field Display Name', 'ENUM']
+            ],
+            enums_names={'test-enum-field-id': ['TEST_ENUM_VALUE']}
+        )
+
+        mock_create_tag_template.assert_called_once()
+
+    @patch(f'{_PATCHED_DATACATALOG_CLIENT}.delete_tag_template')
     def test_delete_tag_template_should_call_client_library_method(self, mock_delete_tag_template):
         DataCatalogHelper().delete_tag_template(None)
         mock_delete_tag_template.assert_called_once()
 
-    @patch(f'{_LOOKED_UP_DATACATALOG_CLIENT}.delete_tag_template')
-    def test_delete_tag_template_should_succeed_nonexistent(self, mock_delete_tag_template):
+    @patch(f'{_PATCHED_DATACATALOG_CLIENT}.delete_tag_template')
+    def test_delete_tag_template_should_handle_nonexistent(self, mock_delete_tag_template):
         mock_delete_tag_template.side_effect = PermissionDenied(message='')
         DataCatalogHelper().delete_tag_template(None)
         mock_delete_tag_template.assert_called_once()
 
-    @patch(f'{_LOOKED_UP_DATACATALOG_CLIENT}.get_tag_template')
+    @patch(f'{_PATCHED_DATACATALOG_CLIENT}.get_tag_template')
     def test_tag_template_exists_should_return_true_existing(self, mock_get_tag_template):
         tag_template_exists = DataCatalogHelper().tag_template_exists(None)
         self.assertTrue(tag_template_exists)
         mock_get_tag_template.assert_called_once()
 
-    @patch(f'{_LOOKED_UP_DATACATALOG_CLIENT}.get_tag_template')
+    @patch(f'{_PATCHED_DATACATALOG_CLIENT}.get_tag_template')
     def test_tag_template_exists_should_return_false_nonexistent(self, mock_get_tag_template):
         mock_get_tag_template.side_effect = PermissionDenied(message='')
         tag_template_exists = DataCatalogHelper().tag_template_exists(None)
@@ -37,6 +211,16 @@ class DataCatalogHelperTest(TestCase):
 
 
 class StringFormatterTest(TestCase):
+
+    def test_format_elements_snakecase_list(self):
+        test_list = ['AA-AA', 'BB-BB']
+        StringFormatter.format_elements_to_snakecase(test_list)
+        self.assertListEqual(['aa_aa', 'bb_bb'], test_list)
+
+    def test_format_elements_snakecase_internal_index(self):
+        test_list = [['AA-AA', 'Test A'], ['BB-BB', 'Test B']]
+        StringFormatter.format_elements_to_snakecase(test_list, internal_index=0)
+        self.assertListEqual([['aa_aa', 'Test A'], ['bb_bb', 'Test B']], test_list)
 
     def test_format_string_to_snakecase_abbreviation(self):
         self.assertEqual('aaa', StringFormatter.format_to_snakecase('AAA'))
--- tests/load_template_google_sheets_test.py
+++ tests/load_template_google_sheets_test.py
@@ -5,30 +5,45 @@ from google.api_core.exceptions import PermissionDenied
 
 from load_template_google_sheets import DataCatalogHelper, StringFormatter
 
-_LOOKED_UP_DATACATALOG_CLIENT = 'load_template_google_sheets.datacatalog_v1beta1.DataCatalogClient'
+_PATCHED_DATACATALOG_CLIENT = 'load_template_google_sheets.datacatalog_v1beta1.DataCatalogClient'
 
 
-@patch(f'{_LOOKED_UP_DATACATALOG_CLIENT}.__init__', lambda self, *args: None)
+@patch(f'{_PATCHED_DATACATALOG_CLIENT}.__init__', lambda self, *args: None)
 class DataCatalogHelperTest(TestCase):
 
-    @patch(f'{_LOOKED_UP_DATACATALOG_CLIENT}.delete_tag_template')
+    @patch(f'{_PATCHED_DATACATALOG_CLIENT}.create_tag_template')
+    def test_create_tag_template_should_handle_described_fields(self, mock_create_tag_template):
+        DataCatalogHelper().create_tag_template(
+            project_id=None,
+            template_id=None,
+            display_name='Test Display Name',
+            fields_descriptors=[
+                ['test-string-field-id', 'Test String Field Display Name', 'STRING'],
+                ['test-enum-field-id', 'Test ENUM Field Display Name', 'ENUM']
+            ],
+            enums_names={'test-enum-field-id': ['TEST_ENUM_VALUE']}
+        )
+
+        mock_create_tag_template.assert_called_once()
+
+    @patch(f'{_PATCHED_DATACATALOG_CLIENT}.delete_tag_template')
     def test_delete_tag_template_should_call_client_library_method(self, mock_delete_tag_template):
         DataCatalogHelper().delete_tag_template(None)
         mock_delete_tag_template.assert_called_once()
 
-    @patch(f'{_LOOKED_UP_DATACATALOG_CLIENT}.delete_tag_template')
-    def test_delete_tag_template_should_succeed_nonexistent(self, mock_delete_tag_template):
+    @patch(f'{_PATCHED_DATACATALOG_CLIENT}.delete_tag_template')
+    def test_delete_tag_template_should_handle_nonexistent(self, mock_delete_tag_template):
         mock_delete_tag_template.side_effect = PermissionDenied(message='')
         DataCatalogHelper().delete_tag_template(None)
         mock_delete_tag_template.assert_called_once()
 
-    @patch(f'{_LOOKED_UP_DATACATALOG_CLIENT}.get_tag_template')
+    @patch(f'{_PATCHED_DATACATALOG_CLIENT}.get_tag_template')
     def test_tag_template_exists_should_return_true_existing(self, mock_get_tag_template):
         tag_template_exists = DataCatalogHelper().tag_template_exists(None)
         self.assertTrue(tag_template_exists)
         mock_get_tag_template.assert_called_once()
 
-    @patch(f'{_LOOKED_UP_DATACATALOG_CLIENT}.get_tag_template')
+    @patch(f'{_PATCHED_DATACATALOG_CLIENT}.get_tag_template')
     def test_tag_template_exists_should_return_false_nonexistent(self, mock_get_tag_template):
         mock_get_tag_template.side_effect = PermissionDenied(message='')
         tag_template_exists = DataCatalogHelper().tag_template_exists(None)
@@ -37,6 +52,16 @@ class DataCatalogHelperTest(TestCase):
 
 
 class StringFormatterTest(TestCase):
+
+    def test_format_elements_snakecase_list(self):
+        test_list = ['AA-AA', 'BB-BB']
+        StringFormatter.format_elements_to_snakecase(test_list)
+        self.assertListEqual(['aa_aa', 'bb_bb'], test_list)
+
+    def test_format_elements_snakecase_internal_index(self):
+        test_list = [['AA-AA', 'Test A'], ['BB-BB', 'Test B']]
+        StringFormatter.format_elements_to_snakecase(test_list, internal_index=0)
+        self.assertListEqual([['aa_aa', 'Test A'], ['bb_bb', 'Test B']], test_list)
 
     def test_format_string_to_snakecase_abbreviation(self):
         self.assertEqual('aaa', StringFormatter.format_to_snakecase('AAA'))
